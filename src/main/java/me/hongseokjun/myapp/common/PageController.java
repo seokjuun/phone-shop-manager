d8aaa30359017495edffe51813bb4e522d71a625
seokjuun <seokjuun@gmail.com>
2025-04-08 17:28:05 +0900

    feat: 회원가입 화면 구현

--- src/main/java/me/hongseokjun/myapp/common/PageController.java
+++ src/main/java/me/hongseokjun/myapp/common/PageController.java
@@ -1,4 +1,10 @@
 package me.hongseokjun.myapp.common;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PageController {
+    @GetMapping("/pages/register")
+    public String register() { return "register";};
 }
